3ab4f3bb0b190a812029f8eeb0eda43124c6bdf9
Ha Yen <hanguyentieuyen@gmail.com>
2023-05-07 01:14:39 +0700

    update code

---
+++ travel_landing_page/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element = {<Home />} > </Route>
+        <Route path='/products' exact element = {<Products />} > </Route>
+        <Route path='/services' exact element = {<Services />} > </Route>
+        <Route path='/sign-up' exact element = {<SignUp />} > </Route>
+
+      </Routes>    
+    </Router>
+    
+    </>
+  );
+}
+
+export default App;
